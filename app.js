657b1a8ddae178442cf42e8ac1777dec90dbe85d
miyabi <miyabi@miyabinoiMac.local>
2026-07-25 16:18:46 +0900

    Build shared MIDI editor foundation

--- app.js
+++ app.js
@@ -1360,5 +1360,6 @@ function loadMusicStudioScript(datasetKey,src,ready){
 }
 loadMusicStudioScript('music-studio-midi','./music-studio-midi.js?v=1.4.0',()=>Boolean(window.MusicStudioMidi))
  .then(()=>loadMusicStudioScript('music-studio-midi-parser','./music-studio-midi-parser.js?v=1.4.0',()=>Boolean(window.MusicStudioMidiParser)))
+ .then(()=>loadMusicStudioScript('music-studio-editor','./music-studio-editor.js?v=1.4.0',()=>Boolean(window.MusicStudioEditor)))
  .then(()=>loadMusicStudioScript('music-studio','./music-studio.js?v=1.4.0',()=>Boolean(window.MusicStudio)))
  .catch(error=>console.error('Music Studio scripts could not be initialized',error));
