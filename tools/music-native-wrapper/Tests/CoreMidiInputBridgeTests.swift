43541be0c3a02b157d2d76e20a9af1b43913a189
puitoybox-cloud <pui.toy.box@gmail.com>
2026-09-14 21:53:26 +0900

    Music Studio: test MIDI 1.0 UMP note decoding

---
+++ tools/music-native-wrapper/Tests/CoreMidiInputBridgeTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import NovaMusicNativeWrapper
+
+final class CoreMidiInputBridgeTests: XCTestCase {
+    func testNoteOnUMPDecodesToThreeMidiBytes() {
+        let word: UInt32 = 0x20903C64 // MT=2, group=0, Note On ch1, note60, velocity100
+        XCTAssertEqual(CoreMidiInputBridge.noteBytes(fromMIDI1UMP: word), [0x90, 60, 100])
+    }
+
+    func testNoteOffUMPDecodesToThreeMidiBytes() {
+        let word: UInt32 = 0x20803C00
+        XCTAssertEqual(CoreMidiInputBridge.noteBytes(fromMIDI1UMP: word), [0x80, 60, 0])
+    }
+
+    func testZeroVelocityNoteOnIsPreservedForJavaScriptRecorderToTreatAsNoteOff() {
+        let word: UInt32 = 0x20903C00
+        XCTAssertEqual(CoreMidiInputBridge.noteBytes(fromMIDI1UMP: word), [0x90, 60, 0])
+    }
+
+    func testNonNoteChannelVoiceMessageIsRejected() {
+        let controlChange: UInt32 = 0x20B0017F
+        XCTAssertNil(CoreMidiInputBridge.noteBytes(fromMIDI1UMP: controlChange))
+    }
+
+    func testNonMidi1ChannelVoiceUMPIsRejected() {
+        let midi2Word: UInt32 = 0x40903C00
+        XCTAssertNil(CoreMidiInputBridge.noteBytes(fromMIDI1UMP: midi2Word))
+    }
+}
