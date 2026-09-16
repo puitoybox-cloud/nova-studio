d3be8a62185fa34f16a3529994309876d1269896
puitoybox-cloud <pui.toy.box@gmail.com>
2026-09-16 15:27:53 +0900

    Pin native iPad verification to PR250 ec0de90

--- tools/music-native-wrapper/Xcode/MusicStudioXcodeRootView.swift
+++ tools/music-native-wrapper/Xcode/MusicStudioXcodeRootView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import WebKit
 
 private let pr250VerificationConfiguration = MusicStudioAppConfiguration(
-    startURL: URL(string: "https://cdn.jsdelivr.net/gh/puitoybox-cloud/nova-studio@d41883fa563c218df677452c74187670d49f5dea/music-studio.html?raw=1")!,
+    startURL: URL(string: "https://cdn.jsdelivr.net/gh/puitoybox-cloud/nova-studio@ec0de90cdb4cb430003cdc3dcf3ce63739f30c5c/music-studio.html?raw=1")!,
     allowedHosts: [
         "cdn.jsdelivr.net",
         "puitoybox-cloud.github.io"
